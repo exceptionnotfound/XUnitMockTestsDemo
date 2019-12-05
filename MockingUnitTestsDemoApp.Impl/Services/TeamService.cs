661c5de655c6bc2168740cafb84a20922050bf0a
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2019-12-05 09:10:31 -0700

    Minor spacing change

--- MockingUnitTestsDemoApp.Impl/Services/TeamService.cs
+++ MockingUnitTestsDemoApp.Impl/Services/TeamService.cs
@@ -9,32 +9,32 @@ using System.Threading.Tasks;
 
 namespace MockingUnitTestsDemoApp.Impl.Services
 {
-public class TeamService : ITeamService
-{
-    private readonly ITeamRepository _teamRepo;
-    private readonly ILeagueRepository _leagueRepo;
-
-    public TeamService(ITeamRepository teamRepo,
-                        ILeagueRepository leagueRepo)
+    public class TeamService : ITeamService
     {
-        _teamRepo = teamRepo;
-        _leagueRepo = leagueRepo;
+        private readonly ITeamRepository _teamRepo;
+        private readonly ILeagueRepository _leagueRepo;
+
+        public TeamService(ITeamRepository teamRepo,
+                           ILeagueRepository leagueRepo)
+        {
+            _teamRepo = teamRepo;
+            _leagueRepo = leagueRepo;
+        }
+
+        public List<Team> Search(TeamSearch search)
+        {
+            //If we are searching for an invalid or unknown League...
+            var isValidLeague = _leagueRepo.IsValid(search.LeagueID);
+            if (!isValidLeague)
+                return new List<Team>(); //Return an empty list.
+
+            //Otherwise get all teams in the specified league...
+            var allTeams = _teamRepo.GetForLeague(search.LeagueID);
+
+            //... and filter them by the specified Founding Date and Direction.
+            if(search.Direction == Enums.SearchDateDirection.OlderThan)
+                return allTeams.Where(x => x.FoundingDate <= search.FoundingDate).ToList();
+            else return allTeams.Where(x => x.FoundingDate >= search.FoundingDate).ToList();
+        }
     }
-
-    public List<Team> Search(TeamSearch search)
-    {
-        //If we are searching for an invalid or unknown League...
-        var isValidLeague = _leagueRepo.IsValid(search.LeagueID);
-        if (!isValidLeague)
-            return new List<Team>(); //Return an empty list.
-
-        //Otherwise get all teams in the specified league...
-        var allTeams = _teamRepo.GetForLeague(search.LeagueID);
-
-        //... and filter them by the specified Founding Date and Direction.
-        if(search.Direction == Enums.SearchDateDirection.OlderThan)
-            return allTeams.Where(x => x.FoundingDate <= search.FoundingDate).ToList();
-        else return allTeams.Where(x => x.FoundingDate >= search.FoundingDate).ToList();
-    }
-}
 }
